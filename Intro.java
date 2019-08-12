fdde92efb80432156059c8ebf485a9a589138b4f
Hines <hinese@rcschools.net>
2019-08-12 10:05:18 -0500

    test x3

--- Intro.java
+++ Intro.java
@@ -3,6 +3,6 @@ public class Intro
 
   public static void main(String[] args)
   {
-    System.out.println("testing testing testing testing");
+    System.out.println("testing testing testing");
   }
 }
